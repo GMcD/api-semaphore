3b2cf13f6d04c02384656d018cfc9be2c42f84bb
GMcD <gary.macdonald@projectscapa.com>
2024-05-02 07:12:30 +0100

    PSql Host for Testing

--- api/app.go
+++ api/app.go
@@ -20,7 +20,7 @@ type App struct {
 
 func (a *App) Default() {
 	a.Initialize(
-		getEnv("APP_DB_HOST", "postgres"),
+		getEnv("APP_DB_HOST", "0.0.0.0"),
 		getEnv("APP_DB_USERNAME", "postgres"),
 		getEnv("APP_DB_PASSWORD", "postgres"),
 		getEnv("APP_DB_NAME", "postgres"))
