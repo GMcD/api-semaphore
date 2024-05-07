19f59a8382abeebcacde3b1bee536ca0ff544187
GMcD <gary.macdonald@projectscapa.com>
2024-05-07 09:11:58 +0100

    DotEnv v3.

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,10 @@ if [ "$2" = "test" ]; then
 
     psql --version
 
-    PGPASS=${DB_PASS}
-    psql -Atx "host=${INSTANCE_UNIX_SOCKET} dbname=${DB_NAME} user=${DB_USER}" -c 'select current_date' 
+    PGPASS=${APP_DB_PASSWORD}
+    psql -Atx "host=${APP_DB_HOST} dbname=${APP_DB_NAME} user=${APP_DB_USERNAME}" -c 'select current_date' 
 
-    APP_DB_HOST=${INSTANCE_UNIX_SOCKET} /usr/local/go/bin/go test .
+    /usr/local/go/bin/go test .
 
     exit $?
 fi
@@ -27,5 +27,5 @@ fi
     echo "Running Mode..."
     /usr/local/go/bin/go version
 
-    APP_DB_HOST=${INSTANCE_UNIX_SOCKET} /usr/local/go/bin/go run .
+    /usr/local/go/bin/go run .
 # fi
