78a5b246f4c406eb137cae112b6ea6f1b4175a51
GMcD <gary.macdonald@projectscapa.com>
2024-05-09 10:16:59 +0100

    Added Env sub module.

---
+++ env_test.go
@@ -0,0 +1,47 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/GMcD/api-semaphore/env"
+)
+
+func TestGetUser(t *testing.T) {
+
+	var v env.Variables
+
+	_, err := v.GetUser()
+	if err != nil {
+		t.Errorf("Failed to get user from GitHub : %v", err)
+	}
+}
+
+func TestGetRepo(t *testing.T) {
+
+	var v env.Variables
+
+	_, err := v.GetRepo(env.GH_repo)
+	if err != nil {
+		t.Errorf("Failed to get repository from GitHub : %v", err)
+	}
+}
+
+func TestGetGithubEnvironment(t *testing.T) {
+
+	var v env.Variables
+
+	vars, err := v.GetGithubEnvironment(env.GH_repo)
+	if err != nil {
+		t.Errorf("Failed to get action variables/secrets from GitHub : %v", err)
+	}
+	if len(*vars) != env.GH_EnvLen {
+		t.Errorf("Wrong count of action variables/secrets : %v", err)
+	}
+}
+
+func TestGetOrganizations(t *testing.T) {
+
+	var v env.Variables
+
+	v.GetOrganizations()
+}
